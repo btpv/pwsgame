04862cd14051ffd6e69d4c832300817c12e9c419
Floris789 <florisprive1@gmail.com>
2025-09-13 13:58:52 +0200

    modified:   Idle.png 	modified:   Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,8 +4,8 @@ pygame.init()
 screen_width, screen_height = 500, 500
 win = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Jour Fun Time")
-width = 40
-height = 60
+width = 128
+height = 128
 vel = 5
 x = screen_width // 2 - width // 2
 y = screen_height - height
@@ -13,14 +13,22 @@ width = 40
 height = 60
 vel = 5
 
-walkright = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
-walkleft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+
+walksheet = pygame.image.load('walk.png')
+walkright = [walksheet.subsurface(pygame.Rect(i * 128, 0, 128, 128)) for i in range(11)]
+walkleft = [walksheet.subsurface(pygame.Rect(i * 128, 128, 128, 128)) for i in range(11)]
+char = pygame.image.load('Idle.png')
 isjumping = False
 jumpcount = 10
 left=False
 right=False
 walkcount=0
 
+def redrawgamewindow():
+    global walkcount
+    win.fill((63, 63, 255))
+    pygame.draw.rect(win, (100, 100, 100), (x, y, width, height))
+    pygame.display.update()   
 run = True
 while run:
     pygame.time.delay(100)
@@ -48,7 +56,5 @@ while run:
         else:
             isjumping = False
             jumpcount = 10
-    win.fill((63, 63, 255))
-    pygame.draw.rect(win, (100, 100, 100), (x, y, width, height))
-    pygame.display.update()       
+    
 pygame.quit()           
